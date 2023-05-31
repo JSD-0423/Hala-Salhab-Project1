b7b212c1fa911e476a59887ff4c6474202679745
halasalhab <halasalhab0@gmail.com>
2023-05-31 15:56:44 +0200

    all ready and done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function showHideFav() {
-    var favourites = document.getElementById("favourite");
+    let favourites = document.getElementById("favourite");
     if (favourites.style.display === "none") {
         favourites.style.display = "block";
     } else {
@@ -8,14 +8,78 @@ function showHideFav() {
 }
 
 function toggleDarkMode() {
-    var body_style = document.body.style;
-    var header_style = document.querySelector("header").style;
-    var textButtons = document.getElementsByClassName("text-button");
-    var banner_style = document.getElementById("banner").style;
-    var searchBar_style = document.getElementById("search-bar").style;
-    var result_cards = document.getElementsByClassName("card");
-    var main_container = document.getElementsByClassName("container");
-    var favourite_style = document.getElementById("favourite").style;
+    let body_style = document.body.style;
+    let header_style = document.querySelector("header").style;
+    let textButtons = document.getElementsByClassName("text-button");
+    let banner_style = document.getElementById("banner").style;
+    let searchBar_style = document.getElementById("search-bar").style;
+    let result_cards = document.getElementsByClassName("card");
+    let card_text_body_secondary = document.querySelectorAll("a")
+    let filter = document.getElementById('filter');
+    let sort = document.getElementById('sort');
+    let favourite_style = document.getElementById("favourite").style;
+    
+    if (body_style.color == "var(--body-text)") {
+        filter.style.backgroundColor = "var(--bg_default-dark)";
+        filter.style.color = "var(--body-text-dark)";
+        sort.style.backgroundColor = "var(--bg_default-dark)";
+        sort.style.color = "var(--body-text-dark)";
+        body_style.color = "var(--body-text-dark)";
+        header_style.backgroundColor = "var(--bg_default-dark)";
+        textButtons[0].style.border = " 1px solid var(--lines-color-dark)";
+        textButtons[0].style.color = "var(--body-text-dark)";
+        textButtons[1].style.border = " 1px solid var(--lines-color-dark)";
+        textButtons[1].style.color = "var(--body-text-dark)";
+        banner_style.backgroundColor = "var(--bg_default-dark)";
+        searchBar_style.backgroundColor = "var(--bg_default-dark)";
+        body_style.backgroundColor = "var(--bg_body-dark)";
+        searchBar_style.boxShadow = "none";
+        favourite_style.backgroundColor = "var(--bg_default-dark)"
+        for (let card of result_cards) {
+            card.style.backgroundColor = "var(--bg_default-dark)";
+            card.style.boxShadow = "none";
+            card.style.color = "var(--body-text-dark)";
+        }
+        for (let text of card_text_body_secondary) {
+            text.style.color = "var(--body-text-dark)";
+        }
+    }
+    else {
+        body_style.color = "var(--body-text)"
+        header_style.backgroundColor = "var(--bg_default)";
+        filter.style.backgroundColor = "var(--bg_default)";
+        filter.style.color = "var(--body-text)";
+        sort.style.backgroundColor = "var(--bg_default)";
+        sort.style.color = "var(--body-text)";
+        textButtons[0].style.border = " 1px solid var(--lines-color)";
+        textButtons[0].style.color = "var(--body-text)";
+        textButtons[1].style.border = " 1px solid var(--lines-color)";
+        textButtons[1].style.color = "var(--body-text)";
+        banner_style.backgroundColor = "var(--bg_default)";
+        searchBar_style.backgroundColor = "var(--bg_default)";
+        body_style.backgroundColor = "var(--bg_body)";
+        searchBar_style.boxShadow = "none";
+        favourite_style.backgroundColor = "var(--bg_default)"
+        for (let card of result_cards) {
+            card.style.backgroundColor = "var(--bg_default)";
+            card.style.boxShadow = "box-shadow: 10px 0 0 0 rgba(191, 190, 190, 0.25)"
+            card.style.color = "var(--body-text)";
+        }
+        for (let text of card_text_body_secondary) {
+            text.style.color = "var(--body-text)";
+        }
+    }
+
+}
+
+function toggleDarkModeDetails(){
+    let body_style = document.body.style;
+    let header_style = document.querySelector("header").style;
+    let textButtons = document.getElementsByClassName("text-button");
+    let banner_style = document.getElementById("banner").style;
+    let topics_table_style = document.getElementById("topics-table").style;
+    let result_cards = document.getElementsByClassName("card");
+    let favourite_style = document.getElementById("favourite").style;
 
     if (body_style.color == "var(--body-text)") {
         body_style.color = "var(--body-text-dark)";
@@ -25,13 +89,14 @@ function toggleDarkMode() {
         textButtons[1].style.border = " 1px solid var(--lines-color-dark)";
         textButtons[1].style.color = "var(--body-text-dark)";
         banner_style.backgroundColor = "var(--bg_default-dark)";
-        searchBar_style.backgroundColor = "var(--bg_default-dark)";
-        main_container[1].style.backgroundColor = "var(--bg_body-dark)";
-        searchBar_style.boxShadow = "none";
-        favourite_style.backgroundColor = "var(--bg_default-dark)"
-        for (let i = 0; i < result_cards.length; i++) {
-            result_cards[i].style.backgroundColor = "var(--bg_default-dark)";
-            result_cards[i].style.boxShadow = "none"
+        body_style.backgroundColor = "var(--bg_body-dark)";
+        topics_table_style.backgroundColor="var(--bg_default-dark)";
+        topics_table_style.color="var(--body-text-dark)";
+        favourite_style.backgroundColor = "var(--bg_default-dark)";
+        for (let card of result_cards) {
+            card.style.backgroundColor = "var(--bg_default-dark)";
+            card.style.boxShadow = "none";
+            card.style.color = "var(--body-text-dark)";
         }
     }
     else {
@@ -42,14 +107,14 @@ function toggleDarkMode() {
         textButtons[1].style.border = " 1px solid var(--lines-color)";
         textButtons[1].style.color = "var(--body-text)";
         banner_style.backgroundColor = "var(--bg_default)";
-        searchBar_style.backgroundColor = "var(--bg_default)";
-        main_container[1].style.backgroundColor = "var(--bg_body)";
-        searchBar_style.boxShadow = "none";
+        body_style.backgroundColor = "var(--bg_body)";
+        topics_table_style.backgroundColor="var(--bg_default)";
+        topics_table_style.color="var(--body-text)";
         favourite_style.backgroundColor = "var(--bg_default)"
-        for (let i = 0; i < result_cards.length; i++) {
-            result_cards[i].style.backgroundColor = "var(--bg_default)";
-            result_cards[i].style.boxShadow = "box-shadow: 10px 0 0 0 rgba(191, 190, 190, 0.25)"
+        for (let card of result_cards) {
+            card.style.backgroundColor = "var(--bg_default)";
+            card.style.boxShadow = "box-shadow: 10px 0 0 0 rgba(191, 190, 190, 0.25)"
+            card.style.color = "var(--body-text)";
         }
     }
-
 }
